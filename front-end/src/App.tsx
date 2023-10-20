4e7709da46b3f31e4cb11d36f2136e7689284cd2
MrPaYu <adrian.adell2002@gmail.com>
2023-10-20 15:56:56 +0200

    front-end spike 0 - refactoring

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,14 +1,12 @@
 import { SyntheticEvent, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
-import { openRouteApi } from './api/openRouteApi';
 import { RoutesMap } from './components/Map';
-import { getEnvVariables } from './helpers/getEnvVariables';
+import { getCoordenates } from './helpers/getCoordenates';
+import { Coords } from './interfaces/Coords';
 import { RootState } from './interfaces/RootState';
 import { searchNewPlace } from './store/map/map.slice';
 
-
-const { VITE_ROUTES_API_KEY } = getEnvVariables();
 
 export const App = () => {
 
@@ -16,28 +14,13 @@ export const App = () => {
   const dispatch = useDispatch();
   const { toponim, coordinates: [ longitud, latitud] } = useSelector( ( state: RootState ) => state.map );
 
-  const getCoordenates = async( event: SyntheticEvent ) => {
+  const onSubmit = async( event: SyntheticEvent ) => {
     
     event.preventDefault();
    
-    const { data } = await openRouteApi.get('/geocode/search', {
-      params: {
-        api_key: VITE_ROUTES_API_KEY,
-        text: inputValue,
-      }
-    });
+    const coordinates: Coords = await getCoordenates( inputValue );
 
-    const { geometry } = data.features[0];
-
-    // console.log({ geometry });
-
-    const { coordinates } = geometry;
-
-    const [ nuevaLongitud, nuevaLatitud ] = coordinates; //? Las cojo al revés porque vienen en el sistema inglés
-
-    // console.log({ nuevaLongitud, nuevaLatitud });
-
-    dispatch( searchNewPlace({ toponim: inputValue, coordinates: [ nuevaLongitud, nuevaLatitud ] }) ); //? Aquí vuelven a ser ingleses
+    dispatch( searchNewPlace({ toponim: inputValue, coordinates }) );
 
   }
 
@@ -49,7 +32,7 @@ export const App = () => {
     <>
       <div className="row d-flex">
         <div className="col-4 d-flex flex-column justify-content-center"  style={{ height: '100vh'}}>
-          <form onSubmit={ getCoordenates }>
+          <form onSubmit={ onSubmit }>
             <div className="input-group">
               <input 
                   type="text" 
