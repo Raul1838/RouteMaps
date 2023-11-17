2372ce1d34e056fe4aa692d6a134bdfac9cec947
MrPaYu <adrian.adell2002@gmail.com>
2023-11-17 18:26:43 +0100

    refactor(it01hu02): login params changed

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,31 +1,20 @@
 import {UserModel} from "../interfaces/UserModel.ts";
 
 export class AuthController {
-    private static _authController: AuthController | undefined;
-
-    private constructor() { }
-
-    public static get authController(): AuthController {
-        if (!this._authController) {
-            this._authController = new AuthController();
-        }
-        return this._authController;
+    public registerUserWithEmailAndPassword( email: string, password: string, displayName: string ): UserModel {
+        throw new Error('Not implemented');
     }
 
-    public registerUserWithEmailAndPassword({ email, password, displayName }): UserModel {
-        return {
-            uid: '',
-            email: '',
-            displayName: ''
-        }
+    public loginWithEmailAndPassword( email: string, password: string ): UserModel {
+        throw new Error('Not implemented');
     }
 
-    public loginWithEmailAndPassword({ email, password }): UserModel {
-        return {
-            uid: '',
-            email: '',
-            displayName: ''
-        }
-    }
+}
 
+let _instance: AuthController;
+export function getAuthController(): AuthController {
+    if (!_instance) {
+        _instance = new AuthController();
+    }
+    return _instance;
 }
