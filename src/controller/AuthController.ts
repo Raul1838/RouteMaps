8aa0f0e20f79ecc27b2b5c4a8b70595bf2d805eb
MrPaYu <adrian.adell2002@gmail.com>
2023-11-17 19:16:43 +0100

    refactor(it01hu02): adjust content to required on HU02

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,10 +1,6 @@
 import {UserModel} from "../interfaces/UserModel.ts";
 
 export class AuthController {
-    public registerUserWithEmailAndPassword( email: string, password: string, displayName: string ): UserModel {
-        throw new Error('Not implemented');
-    }
-
     public loginWithEmailAndPassword( email: string, password: string ): UserModel {
         throw new Error('Not implemented');
     }
--- tests/auth/auth.test.tsx
+++ tests/auth/auth.test.tsx
@@ -1,4 +1,4 @@
-import {AuthController} from "../../src/controller/AuthController";
+import {AuthController, getAuthController} from "../../src/controller/AuthController";
 import {UserModel} from "../../src/interfaces/UserModel";
 import {AuthException, AuthExceptionMessages} from "../../src/exceptions/AuthException";
 
@@ -6,52 +6,17 @@ describe('Tests sobre gestión de usuarios en Firebase', () => {
 
     let authController: AuthController;
 
-    beforeEach(() => authController = AuthController.authController)
+    beforeEach(() => authController = getAuthController());
 
-    test('HU01 - E1 - registro exitoso', () => {
-
-        const testUser = {
-            email: 'usuario.prueba@test.com',
-            password: '123456789',
-            displayName: 'Usuario Prueba'
-        }
-
-        const user: UserModel = authController.registerUserWithEmailAndPassword( testUser );
-
-        expect(user).toBeTruthy();
-        expect(user.email).toBe(testUser.email);
-        expect(user.displayName).toBe(testUser.displayName);
-    });
-
-    test('HU01 - E3 - registro fallido con email inválido', () => {
-
-        const testUser = {
-            email: 'usuario.pruebatest.com',
-            password: '123456789',
-            displayName: 'Usuario Prueba'
-        }
-
-        try {
-            authController.registerUserWithEmailAndPassword( testUser );
-            throw new Error();
-        } catch (error) {
-            if (error instanceof AuthException) {
-                expect(error.message).toBe(AuthExceptionMessages.InvalidRegister);
-            } else {
-                throw new Error('Lanzada una excepción no controlada');
-            }
-        }
-
-    });
 
     test('HU02 - E1 - Login exitoso', () => {
 
         const testUser = {
-            email: 'usuario.prueba@test.com',
+            email: 'usuario.permanente@test.com',
             password: '123456789',
         }
 
-        const user: UserModel = authController.loginWithEmailAndPassword( testUser );
+        const user: UserModel = authController.loginWithEmailAndPassword( testUser.email, testUser.password );
 
         expect(user).toBeTruthy();
         expect(user.email).toBe(testUser.email);
@@ -62,12 +27,12 @@ describe('Tests sobre gestión de usuarios en Firebase', () => {
     test('HU02 - E3 - Login fallido con contraseña inválida', () => {
 
         const testUser = {
-            email: 'usuario.prueba@test.com',
-            password: '123456789',
+            email: 'usuario.permanente@test.com',
+            password: '123',
         }
 
         try {
-            authController.loginWithEmailAndPassword( testUser );
+            authController.loginWithEmailAndPassword( testUser.email, testUser.password );
             throw new Error();
         } catch (error) {
             if (error instanceof AuthException) {
