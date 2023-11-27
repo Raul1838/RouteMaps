cc38bbcda5e905beaa0d89c5eea1ab8c13c03c1a
Raul <al394796@uji.es>
2023-11-27 18:49:38 +0100

    feat(it01h05): Añadir cóodigo para pasar prueba E04

--- tests/helpers/MockApiPlacesService.ts
+++ tests/helpers/MockApiPlacesService.ts
@@ -1,3 +1,4 @@
+import APINotAvailableExeption from "../../src/exceptions/APINotAvailableExeption";
 import APIPlacesInterface from "../../src/interfaces/APIPlacesInterface";
 import { Coords } from "../../src/interfaces/Coords";
 import Place from "../../src/interfaces/Place";
@@ -6,7 +7,9 @@ import Place from "../../src/interfaces/Place";
 
 export default class MockAPIPlacesService implements APIPlacesInterface {
     async getPlaceByCoord(coordinates: Coords): Promise<Place> {
-
+        if ((coordinates.Longitud === -0.0576800) && (coordinates.Latitud === 0)) {
+            throw new APINotAvailableExeption();
+        }
         if ((coordinates!.Longitud! < 100) && (coordinates!.Latitud! < 100)) {
             return {
                 Latitud: coordinates.Latitud!,
@@ -16,10 +19,10 @@ export default class MockAPIPlacesService implements APIPlacesInterface {
             };
         }
         return {
-                Latitud: coordinates.Latitud!,
-                Longitud: coordinates.Longitud!,
-                Nombre: "",
-                Favorito: false
+            Latitud: coordinates.Latitud!,
+            Longitud: coordinates.Longitud!,
+            Nombre: "",
+            Favorito: false
         };
     }
 
--- tests/test.ts
+++ tests/test.ts
@@ -53,23 +53,23 @@ describe('Pruebas de la Iteración 1', () => {
                 // Given
                 expect.assertions(1);
                 placesController.setPlaces([{
-                  Nombre: "Valencia",
-                  Longitud: -0.3773900,
-                  Latitud: 39.4697500,
-                  Favorito: false
+                    Nombre: "Valencia",
+                    Longitud: -0.3773900,
+                    Latitud: 39.4697500,
+                    Favorito: false
                 }]);
-                
+
                 // When
-                  await placesController.addPlace({
+                await placesController.addPlace({
                     Longitud: -0.0576800,
                     Latitud: "adfd"
-                  }).then( () => fail('Expected an error to be thrown')).catch((error) => expect(error).toBeInstanceOf(IllegalArgumentException));
-                  // If no error is thrown, fail the test
+                }).then(() => fail('Expected an error to be thrown')).catch((error) => expect(error).toBeInstanceOf(IllegalArgumentException));
+                // If no error is thrown, fail the test
 
-              });
-              
-              
-              
+            });
+
+
+
 
             test('E04 - La API no se encuentra disponible.', async () => {
                 expect.assertions(1);
@@ -81,55 +81,52 @@ describe('Pruebas de la Iteración 1', () => {
                     Favorito: false
                 }]);
                 // When
-                const funcion = async () => {
-                    await placesController.addPlace({
-                        Longitud: -0.0576800,
-                        Latitud: 39.4697500,
-                    });
-                }
 
-                expect(funcion).toThrow(APINotAvailableExeption);
+                await placesController.addPlace({
+                    Longitud: -0.0576800,
+                    Latitud: 0,
+                }).then(() => fail('Expected an error to be thrown')).catch((error) => expect(error).toBeInstanceOf(APINotAvailableExeption));
 
             });
         });
 
 
-            describe('HU06 - Dar de alta un lugar de interés con topónimo', () => {
-                test.todo('Se insertan unas coordenadas válidas.');
-                test.todo('Las coordenadas insertadas no son válidas.');
-            });
-            describe('HU07 - Consultar lista de lugares de interés', () => {
-                test.todo('Existe una lista con lugares dados de alta.');
-                test.todo('No contamos con una lista con lugares dados de alta.');
-            });
-            describe('HU08 - Eliminar un lugar de interés', () => {
-                test.todo('Existe el lugar que se quiere eliminar y está dado de alta en la lista de lugares de interés.');
-                test.todo('Hay lugares dados de alta, pero no se encuentra el que se quiere eliminar.');
-                test.todo('No contamos con una lista con lugares dados de alta.');
-            });
+        describe('HU06 - Dar de alta un lugar de interés con topónimo', () => {
+            test.todo('Se insertan unas coordenadas válidas.');
+            test.todo('Las coordenadas insertadas no son válidas.');
         });
-
-        describe('Vehicles', () => {
-            describe('HU09 - Dar de alta un vehículo', () => {
-                test.todo('Se añade un vehículo no registrado anteriormente con datos correctos.');
-                test.todo('El vehículo ya se encuentra dado de alta.');
-                test.todo('Los datos del vehículo no son correctos.');
-            });
-            describe('HU10 - Consultar lista de vehículos', () => {
-                test.todo('Existe una lista con vehículos dados de alta.');
-                test.todo('No contamos con una lista con vehículos dados de alta.');
-            });
-            describe('HU11 - Eliminar un vehículo', () => {
-                test.todo('Existe una lista con vehículos dados de alta y existe el vehículo que se quiere eliminar.');
-                test.todo('Existe una lista con vehículos dados de alta pero no existe el vehículo que se quiere eliminar.');
-                test.todo('No hay vehículos dados de alta.');
-
-            });
-            describe('HU12 - Modificar datos de un vehículo', () => {
-                test.todo('Existe una lista con vehículos dados de alta y existe el vehículo que se quiere modificar.');
-                test.todo('Existe una lista con vehículos dados de alta pero no existe el vehículo que se quiere modificar.');
-                test.todo('No hay vehículos dados de alta.');
-
-            });
+        describe('HU07 - Consultar lista de lugares de interés', () => {
+            test.todo('Existe una lista con lugares dados de alta.');
+            test.todo('No contamos con una lista con lugares dados de alta.');
+        });
+        describe('HU08 - Eliminar un lugar de interés', () => {
+            test.todo('Existe el lugar que se quiere eliminar y está dado de alta en la lista de lugares de interés.');
+            test.todo('Hay lugares dados de alta, pero no se encuentra el que se quiere eliminar.');
+            test.todo('No contamos con una lista con lugares dados de alta.');
         });
     });
+
+    describe('Vehicles', () => {
+        describe('HU09 - Dar de alta un vehículo', () => {
+            test.todo('Se añade un vehículo no registrado anteriormente con datos correctos.');
+            test.todo('El vehículo ya se encuentra dado de alta.');
+            test.todo('Los datos del vehículo no son correctos.');
+        });
+        describe('HU10 - Consultar lista de vehículos', () => {
+            test.todo('Existe una lista con vehículos dados de alta.');
+            test.todo('No contamos con una lista con vehículos dados de alta.');
+        });
+        describe('HU11 - Eliminar un vehículo', () => {
+            test.todo('Existe una lista con vehículos dados de alta y existe el vehículo que se quiere eliminar.');
+            test.todo('Existe una lista con vehículos dados de alta pero no existe el vehículo que se quiere eliminar.');
+            test.todo('No hay vehículos dados de alta.');
+
+        });
+        describe('HU12 - Modificar datos de un vehículo', () => {
+            test.todo('Existe una lista con vehículos dados de alta y existe el vehículo que se quiere modificar.');
+            test.todo('Existe una lista con vehículos dados de alta pero no existe el vehículo que se quiere modificar.');
+            test.todo('No hay vehículos dados de alta.');
+
+        });
+    });
+});
