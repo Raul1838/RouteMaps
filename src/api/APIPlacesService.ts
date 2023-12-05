22e4acddf85c97e2892355ad2f612e670501f003
Raul <al394796@uji.es>
2023-12-05 08:34:12 +0100

    fix(it01h05): Arreglar API url e Interfaz de respuesta

--- src/api/APIPlacesService.ts
+++ src/api/APIPlacesService.ts
@@ -1,8 +1,8 @@
 import APINotAvailableExeption from "../exceptions/APINotAvailableExeption";
 import { getEnvVariables } from "../helpers/getEnvVariables";
 import APIPlacesInterface from "../interfaces/APIPlacesInterface";
+import GetPlaceByCoord from "../interfaces/OpenRoutingInterface";
 import { Coords } from "../interfaces/Coords";
-import { GetPlaceByCoord } from "../interfaces/OpenRoutingInterface";
 import Place from "../interfaces/Place";
 import { openRouteApi } from "./openRouteApi";
 
@@ -16,13 +16,13 @@ export default class APIPlacesService implements APIPlacesInterface {
             Nombre: "",
             Favorito: false
         };
-        try{
-        var res: GetPlaceByCoord | undefined = await openRouteApi.get(`/geocode/reverse?${VITE_ROUTES_API_KEY}&point.lon=${coordinates.Longitud!}&point.lat=${coordinates.Latitud!}`);
+        try {
+            var res: GetPlaceByCoord | undefined = await openRouteApi.get(`/geocode/reverse?api_key=${VITE_ROUTES_API_KEY}&point.lon=${coordinates.Longitud!}&point.lat=${coordinates.Latitud!}`);
         } catch {
             throw new APINotAvailableExeption();
         }
-        if (res?.features[0].properties.name !== undefined) {
-            result = { ...result, Nombre: res?.features[0].properties.name }
+        if (res?.data?.features[0].properties.name !== undefined) {
+            result = { ...result, Nombre: res?.data?.features[0].properties.locality }
         }
         if (result.Nombre === undefined) {
             throw new Error("");
