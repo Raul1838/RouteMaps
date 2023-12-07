258adeefbcebc93c7a23f024f5a9054c0b93be2c
DIEGOPC\diego <al404832@uji.es>
2023-12-07 13:15:10 +0100

    feat(it01hu02): implemented firebase for testing

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,23 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebaseProdConfig from './firebaseProdConfig';
+import firebaseTestConfig from './firebaseTestConfig';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCVOJPZxYP9RoB-I_3kdN-36gv9JdHr1cM",
-  authDomain: "routemaps-34d2b.firebaseapp.com",
-  projectId: "routemaps-34d2b",
-  storageBucket: "routemaps-34d2b.appspot.com",
-  messagingSenderId: "528232130057",
-  appId: "1:528232130057:web:25c5779566edc1de92bad8"
-};
+const firebaseConfig = process.env.NODE_ENV === 'test' ? firebaseTestConfig : firebaseProdConfig;
 
-// Initialize Firebase
-export const FirebaseApp = initializeApp( firebaseConfig );
-
-export const FirebaseAuth = getAuth( FirebaseApp );
-
-export const FirebaseDB = getFirestore( FirebaseApp );
+export const FirebaseApp = initializeApp(firebaseConfig);
+export const FirebaseAuth = getAuth(FirebaseApp);
+export const FirebaseDB = getFirestore(FirebaseApp);
