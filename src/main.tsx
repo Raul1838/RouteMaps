d5d343bfdf505e604f940edd7b1a66d5f3cb3705
MrPaYu <adrian.adell2002@gmail.com>
2023-12-12 20:51:13 +0100

    feat(it01): resolved conflicts

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import './index.css'
+import {App} from './App'
 import {BrowserRouter} from "react-router-dom"; // or "const mapboxgl = require('mapbox-gl');"
 
 // const { VITE_MAP_API_KEY } = getEnvVariables();
