ed80820a00ffc210661fbd0d72698ec0b3dc8166
DiegoSolso <diego.solsona24@gmail.com>
2023-12-12 21:02:51 +0100

    Update FirebaseService.ts

--- src/firebase/FirebaseService.ts
+++ src/firebase/FirebaseService.ts
@@ -1,9 +1,27 @@
-import {deleteUser, signInWithEmailAndPassword} from "firebase/auth";
+import {createUserWithEmailAndPassword, deleteUser, signInWithEmailAndPassword, updateProfile} from "firebase/auth";
 import {FirebaseAuth} from "./config";
 import {UserModel} from "../interfaces/UserModel.ts";
 import {AuthException, AuthExceptionMessages} from "../exceptions/AuthException.ts";
 
 export class FirebaseService {
+
+    async createUserWithEmailAndPassword(email: string, password: string, displayName: string): Promise<UserModel> {
+        let resp;
+        try {
+            resp = await createUserWithEmailAndPassword(FirebaseAuth, email, password);
+        } catch ( error ) {
+            throw new AuthException(AuthExceptionMessages.InvalidRegister);
+        }
+        const { uid } = resp.user;
+        if (FirebaseAuth.currentUser) {
+            await updateProfile(FirebaseAuth.currentUser, {displayName});
+        }
+        return {
+            uid,
+            email,
+            displayName
+        }
+    }
 
     async startLoginWithEmailAndPassword(email: string, password: string): Promise<UserModel> {
         let resp;
