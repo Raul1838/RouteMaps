783c764e8177d480367a6c2956d9d89001a175dc
MrPaYu <adrian.adell2002@gmail.com>
2023-12-21 17:50:47 +0100

    refactor(it02): env variables for dev and test

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,10 @@
 // En caso de necesitar la implementación del FetchAPI
 import 'whatwg-fetch'; // <-- yarn add whatwg-fetch
+
+require('dotenv').config({
+    path: '.env'
+});
+
+jest.mock('./src/helpers/getEnvVariables.ts', () => ({
+    getEnvVariables: () => ({ ...process.env })
+}));
