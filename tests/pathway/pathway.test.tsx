32b9801a7420101455b6cffe9e203fb378d92f0d
MrPaYu <adrian.adell2002@gmail.com>
2023-12-26 20:19:05 +0100

    test(it02-hu23): acceptance test and basic functionality implemented

--- tests/pathway/pathway.test.tsx
+++ tests/pathway/pathway.test.tsx
@@ -2,12 +2,18 @@ import {Coords} from "../../src/interfaces/Coords";
 import {getPathwayController, PathwayController} from "../../src/controller/PathwayController";
 import {PathwayException, PathWayExceptionMessages} from "../../src/exceptions/PathwayException";
 import {Pathway} from "../../src/interfaces/Pathway";
+import VehiclesController, {getVehiclesController} from "../../src/controller/VehiclesController";
+import VehicleNotFoundException from "../../src/exceptions/VehicleNotFoundException";
 
 describe('Tests sobre gestión de rutas', () => {
 
     let pathwayController: PathwayController;
+    let vehiclesController: VehiclesController;
 
-    beforeAll(() => pathwayController = getPathwayController());
+    beforeAll(() => {
+        pathwayController = getPathwayController();
+        vehiclesController = getVehiclesController();
+    });
 
     test('HU13 - E1 - Ruta posible' , async () => {
         const from: Coords = {
@@ -43,6 +49,25 @@ describe('Tests sobre gestión de rutas', () => {
         } catch (error) {
             if( error instanceof PathwayException ) {
                 expect(error.message).toBe(PathWayExceptionMessages.InvalidPathway);
+            } else {
+                throw new Error('Lanzada una excepción no controlada');
+            }
+        }
+    });
+
+    test('HU23 - E1 - Existe el vehículo a establecer por defecto', async () => {
+        const vehicleId: number = 123;
+        vehiclesController.setDefaultVehicle(vehicleId);
+    });
+
+    test('HU23 - E2 - No existe el vehículo a establecer por defecto', async () => {
+        const vehicleId: number = 321;
+        try {
+            vehiclesController.setDefaultVehicle(vehicleId);
+            throw new Error();
+        } catch (error) {
+            if( error instanceof VehicleNotFoundException ) {
+                expect(error.message).toBe('El vehículo no existe');
             } else {
                 throw new Error('Lanzada una excepción no controlada');
             }
