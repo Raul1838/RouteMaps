40a203930bebdda3d03f2905aea09fe82d0e9031
Raul <al394796@uji.es>
2023-12-29 16:00:38 +0100

    feat(it02h15): Implementar código para añadir rutas a Firebase

--- src/services/FirebaseService.ts
+++ src/services/FirebaseService.ts
@@ -1,8 +1,13 @@
-import {createUserWithEmailAndPassword, deleteUser, signInWithEmailAndPassword, updateProfile} from "firebase/auth";
-import {FirebaseAuth, FirebaseDB} from "../firebase/config.ts";
-import {UserModel} from "../interfaces/UserModel.ts";
-import {AuthException, AuthExceptionMessages} from "../exceptions/AuthException.ts";
-import {doc, getDoc, setDoc} from "firebase/firestore/lite";
+import { createUserWithEmailAndPassword, deleteUser, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { FirebaseAuth, FirebaseDB } from "../firebase/config.ts";
+import { UserModel } from "../interfaces/UserModel.ts";
+import { AuthException, AuthExceptionMessages } from "../exceptions/AuthException.ts";
+import { doc, getDoc, setDoc } from "firebase/firestore/lite";
+import { Pathway } from "../interfaces/Pathway.ts";
+import { arrayUnion, updateDoc } from "firebase/firestore";
+import { FirebasePathways } from "../interfaces/FirebaseDB/FirebasePathways.ts";
+import Combustible from "../enums/Combustible.ts";
+import { FirebaseFuelPrice } from "../interfaces/FirebaseDB/FirebaseFuelPrice.ts";
 
 export class FirebaseService {
 
@@ -10,12 +15,12 @@ export class FirebaseService {
         let resp;
         try {
             resp = await createUserWithEmailAndPassword(FirebaseAuth, email, password);
-        } catch ( error ) {
+        } catch (error) {
             throw new AuthException(AuthExceptionMessages.InvalidRegister);
         }
         const { uid } = resp.user;
         if (FirebaseAuth.currentUser) {
-            await updateProfile(FirebaseAuth.currentUser, {displayName});
+            await updateProfile(FirebaseAuth.currentUser, { displayName });
         }
         return {
             uid,
@@ -40,7 +45,7 @@ export class FirebaseService {
     }
 
     async startLogout(): Promise<void> {
-        if( FirebaseAuth.currentUser === null ) {
+        if (FirebaseAuth.currentUser === null) {
             throw new AuthException(AuthExceptionMessages.InvalidLogout);
         }
         await FirebaseAuth.signOut().catch(() => {
@@ -58,8 +63,8 @@ export class FirebaseService {
         }
     }
 
-    async storeValue(path: string, value : any) {
-        
+    async storeValue(path: string, value: any) {
+
     }
 
     async setDefaultVehicle(vehicleId: number, userId: string): Promise<void> {
@@ -75,4 +80,33 @@ export class FirebaseService {
         return docSnap.data();
     }
 
+    async storePathway(pathway: Pathway, userId: string): Promise<void> {
+        const docRef = doc(FirebaseDB, userId, 'pathways');
+        await setDoc(docRef, { pathways: arrayUnion(pathway) }, { merge: true })
+    }
+
+    async getPathways(userId: string) {
+        const docRef = doc(FirebaseDB, `${userId}`, 'pathways');
+        const docSnap = await getDoc(docRef);
+        if (!docSnap.exists())
+            throw new Error('No such document!');
+        return docSnap.data();
+    }
+
+    async storeFuelPrice(fuel: Combustible, fuelPrice: number) {
+        const docRef = doc(FirebaseDB, 'fuel', `${fuel}`);
+        const updateData = {
+            price: fuelPrice,
+            date: new Date()
+        };
+        await setDoc(docRef, updateData, { merge: true });
+    }
+
+    async getFuelPriceAndDate(fuel: Combustible) {
+        const docRef = doc(FirebaseDB, 'fuel', `${fuel}`);
+        const docSnap = await getDoc(docRef);
+        if (!docSnap.exists())
+            throw new Error('No such document!');
+        return docSnap.data();
+    }
 }
