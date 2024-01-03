f2bbf55e1a0ec416229f98d2bf7d981ed1178c0f
MrPaYu <adrian.adell2002@gmail.com>
2024-01-03 20:53:08 +0100

    refactor(it03): merge bugs fixed

--- src/interfaces/VehiclesInterface.ts
+++ src/interfaces/VehiclesInterface.ts
@@ -3,7 +3,7 @@ import Vehicle from "./Vehicle";
 export default interface VehiclesInterface {
     addVehicle(paramVehicle: Vehicle, userId?: string): Promise<boolean>;
     getVehicles(): Vehicle[];
-    deleteVehicle(plate: string, userId?: string): boolean;
+    deleteVehicle(plate: string, userId?: string): Promise<boolean>;
     setVehicles(vehicles: Vehicle[]): void;
     modifyVehicle(paramVehicle: Vehicle, userId?: string): Promise<boolean>;
     toggleFavourite({ plate }: { plate: string }, userId?: string): boolean;
