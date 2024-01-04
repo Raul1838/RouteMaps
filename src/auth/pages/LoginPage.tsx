70294069d0babad98a5327c2346b97ad9adf7453
MrPaYu <adrian.adell2002@gmail.com>
2024-01-04 17:22:11 +0100

    refactor(it03): fixed login image to only fill max height and max width, not more

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -79,7 +79,9 @@ export const LoginPage = () => {
                     </div>
                 </div>
                 <div className="col-6">
-                    <img src="https://i.pinimg.com/originals/89/54/f8/8954f88c60dfde5438e2a5233579b580.jpg"  alt="map image | auth"/>
+                    <img src="https://i.pinimg.com/originals/89/54/f8/8954f88c60dfde5438e2a5233579b580.jpg"
+                         alt="RouteMaps welcome image | auth"
+                         style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'contain' }}/>
                 </div>
             </div>
         </>
