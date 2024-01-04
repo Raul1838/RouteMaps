52869dc2e43eba299794fffae8c4cff5a62ead97
Raul <al394796@uji.es>
2024-01-04 23:36:07 +0100

    feat(it03): Build application

--- src/vehicles/router/VehiclesRouter.tsx
+++ src/vehicles/router/VehiclesRouter.tsx
@@ -1,22 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom"
-import VehiclesController from '../../controller/VehiclesController'
-import VehiclesViewModel from '../viewModel/VehiclesViewModel'
 import AddVehicleView from '../pages/AddVehicleView'
 import GetVehiclesView from '../pages/GetVehiclesView'
 import ModifyVehicleView from "../pages/ModifyVehicleView"
-import DeleteVehicleView from "../pages/DeleteVehicleView"
-import { FirebaseService } from "../../services/FirebaseService"
 
 
-const vehiclesController = new VehiclesController(new FirebaseService);
-const vehiclesViewModel = new VehiclesViewModel(vehiclesController);
 
 export const VehiclesRouter = () => {
     return (
         <Routes>
             <Route path='/addVehicle' element={<AddVehicleView></AddVehicleView>} />
             <Route path='/modifyVehicle/:plate' element={<ModifyVehicleView></ModifyVehicleView>} />
-            <Route path='/deleteVehicle/:plate' element={<DeleteVehicleView vehiclesViewModel={vehiclesViewModel}></DeleteVehicleView>} />
             <Route path='/getVehicles' element={<GetVehiclesView></GetVehiclesView>} />
             <Route path='/*' element={<Navigate to={'/vehicles/getVehicles'} />} />
         </Routes>
